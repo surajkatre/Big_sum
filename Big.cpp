85df38bdc40ef4ca556f6430087003812a0df530
surajkatre <55243782+surajkatre@users.noreply.github.com>
2021-11-07 15:53:53 +0530

    Create Big.cpp

---
+++ Big.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,i;
+    cin>>n;
+    long long int a[n],sum=0;
+    for(i=0;i<n;++i){
+        cin>>a[i];
+    }
+    for (i=0; i<n; ++i) {
+        sum+= a[i];
+    } 
+    cout<<sum;  
+}
